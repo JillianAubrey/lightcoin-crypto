ee74f265495c20f5b850a8696a2a5a6dff610ed1
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-30 19:05:19 +0000

    Completed Light coin activity

--- index.js
+++ index.js
@@ -1,29 +1,66 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
-
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    return this.transactions.reduce((bal, t) => bal += t.value, 0);
   }
-
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    if (!this.isAllowed()) {
+      return false;
+    }
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
+  }
+  isAllowed() {
+    return this.amount > 0;
+  }
+}
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+  isAllowed() {
+    return super.isAllowed() && this.amount < this.account.balance;
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+}
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("Jillian's Account", 500);
+console.log('Starting balance is 0:', myAccount.balance);
+const t0 = new Deposit(500, myAccount);
+console.log('Status of depositing $500', t0.commit());
+console.log('Current balance:', myAccount.balance);
+const t1 = new Withdrawal(50.25, myAccount);
+console.log('Status of withdrawing $50.25:',t1.commit());
+console.log('Current balance:', myAccount.balance);
+const t3 = new Deposit(-120.00, myAccount);
+console.log('Status of Depositing -$120:',t3.commit());
+console.log('Current balance:', myAccount.balance);
+const t4 = new Withdrawal(1000, myAccount);
+console.log('Status of Withdrawing $1000:',t4.commit());
+console.log('Current balance:', myAccount.balance);
+const t5 = new Withdrawal(-1, myAccount);
+console.log('Status of Withdrawing -$1:',t5.commit());
+console.log('Current balance:', myAccount.balance);
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
+console.log('Transaction list');
+console.log(myAccount.transactions);
